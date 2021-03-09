36f0ff9dc18aaf9e7f274fc0c3a6d83b0b1379bb
Issa Suleiman <issa_suleiman008@outlook.com>
2021-03-09 14:56:33 +0300

    Drop and pick

--- Scenes/People.gd
+++ Scenes/People.gd
@@ -2,15 +2,17 @@ extends KinematicBody2D
 
 signal picked
 
+var motion = Vector2(0,1)
+const GRAVITY = 100
+
+
 
 func _physics_process(_delta):
-	
-	
-	if $Pickup.enabled and $Pickup.is_colliding():
+	move_and_slide(motion)
+	motion.y = GRAVITY
+	if $Pickup.is_colliding():
 		emit_signal("picked")
 		queue_free()
-
-
 
 func _on_Level_1_enable():
 	$Pickup.enabled = true
